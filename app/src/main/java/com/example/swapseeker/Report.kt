7569f09fd2f21278a8a941b560fddcc1e1c1b7d3
Sindhu Vardhan <sindhuvardhan333@gmail.com>
2024-04-01 19:27:14 +0530

    Revert "SwapSeeker Project"
    This reverts commit 86ba96cb37479aa800943a62ff3fbfcc9b4d1710.

--- app/src/main/java/com/example/swapseeker/Report.kt
+++
@@ -1,6 +0,0 @@
-package com.example.swapseeker
-
-data class Report(
-    val reportId: String,
-    val productId: String
-)
--- app/src/main/java/com/example/swapseeker/UserDetails.kt
+++
@@ -1,9 +0,0 @@
-package com.example.swapseeker
-
-data class UserDetails(
-    val username: String,
-    val email: String,
-    val phone: String,
-    val country: String,
-    val profileImageUrl: String
-)
